026af65e77bed35b1bb1f9ccd885dcab96f82069
Olha <olyasavvz@gmail.com>
2024-07-08 17:41:55 +0300

    done

--- js/task-1.js
+++
@@ -1,7 +0,0 @@
-const categories = document.querySelectorAll('.item');
-console.log(`Number of categories: ${categories.length}`);
-
-categories.forEach(category => {
-  console.log(`Category: ${category.firstElementChild.textContent}`);
-  console.log(`Elements: ${category.lastElementChild.childElementCount}`);
-});
